1346821d8b6945d94fb29c301da7a034bcdafc61
lexis-webb <lexiswebb.design@gmail.com>
2024-11-07 14:34:37 -0500

    add

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Part 1
+
 // Loop through all numbers from 1 to 100.
 for( let i = 1; i <= 100; i++){
     console.log(i)
@@ -11,7 +13,19 @@ while(num % 3 === 0){
 }
 
 // If a number is divisible by 5, log “Buzz.”
+let div5 = 20
+
+while (div5 % 5 === 0){
+    console.log("Buzz")
+    break;
+}
 
 // If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+let divisible = 15
+
+while( divisible % 3 === 0 && divisible % 5 === 0){
+    console.log("Fizz Buzz")
+    break;
+}
 
 // If a number is not divisible by either 3 or 5, log the number.
