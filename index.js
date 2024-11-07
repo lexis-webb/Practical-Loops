11f9fb2d0f0d7b70ed77740fa9f5debe2f4091af
lexis-webb <lexiswebb.design@gmail.com>
2024-11-07 14:19:13 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Loop through all numbers from 1 to 100.
+for( let i = 1; i <= 100; i++){
+    console.log(i)
+}
+// If a number is divisible by 3, log “Fizz.”
+let  num= 21
+
+while(num % 3 === 0){
+    console.log("Fizz")
+    break;
+}
+
+// If a number is divisible by 5, log “Buzz.”
+
+// If a number is divisible by both 3 and 5, log “Fizz Buzz.”
+
+// If a number is not divisible by either 3 or 5, log the number.
