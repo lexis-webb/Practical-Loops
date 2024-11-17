47c5a9c805e2ca27d2b4233b16fea7ade1246cdf
lexis-webb <lexiswebb.design@gmail.com>
2024-11-16 21:12:24 -0500

    update

--- index.js
+++ index.js
@@ -66,3 +66,23 @@ function nextNum (n){
 let n = 12
 nextNum(n);
 
+// Loop through the characters of a given CSV string.
+// Store each “cell” of data in a variable.
+// When you encounter a comma, move to the next cell.
+// When you encounter the “\r\n” sequence, move to the next “row.”
+// Log each row of data.
+
+
+function loopThrough(csvString) {
+    const rows = [];
+    //split into rows
+    csvString.split(/\r?\n/).forEach(row => {
+        const cells = row.split(',');
+        rows.push(cells);
+    });
+    
+    // Log each row
+    rows.forEach(row => console.log(row));
+}
+let csvString = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232"
+loopThrough(csvString);
